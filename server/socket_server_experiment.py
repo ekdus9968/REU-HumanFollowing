dae0880b7e17b4fee8a34396551c0abd18be7ac7
Laray Lopez <166071733+Larayjay@users.noreply.github.com>
2026-08-11 12:06:53 -0400

    lateral kp=15

--- server/socket_server_experiment.py
+++ server/socket_server_experiment.py
@@ -128,7 +128,7 @@ picam2.configure(picam2.create_video_configuration(
 picam2.start()
 # ──────────────────────────────────────────────────────
 
-lateral_pid = PID(Kp=10.0,  Ki=0.0, Kd=2.0,   output_limit=MAX_SPEED)
+lateral_pid = PID(Kp=15.0,  Ki=0.0, Kd=2.0,   output_limit=MAX_SPEED)
 forward_pid = PID(Kp=0.02, Ki=0.0, Kd=0.005, output_limit=MAX_SPEED)
 
 # ── Global State ───────────────────────────────────────
